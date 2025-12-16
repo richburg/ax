df09e8264883b534f6474cb6a6b6f6da4f19ac5d
Dylan Ashbourne <ashbourne.dylan@gmail.com>
2025-12-17 00:57:50 +0100

    refactor: name changes to afk commands

--- server/commands/afk.py
+++ server/commands/afk.py
@@ -5,15 +5,15 @@ from server.models import Client, Payload
 
 @expect_a_nick()
 @expect_args(0)
-async def handle_toggleafk(client: Client, _):
+async def handle_toggle_afk(client: Client, _):
     """Toggle your AFK status"""
     client.away = not client.away
-    await client.write("TOGGLEAFK_OK")
+    await client.write("TOGGLE_AFK_OK")
 
 
 @expect_a_nick()
 @expect_args(1)
-async def handle_status(client: Client, payload: Payload):
+async def handle_check_afk(client: Client, payload: Payload):
     """Check if someone is AFK"""
     target_nick = payload.args[0]
     target = get_client_by_nick(target_nick)
@@ -22,4 +22,4 @@ async def handle_status(client: Client, payload: Payload):
         await client.write("USER_NOT_FOUND")
         return
 
-    await client.write(f"STATUS_OK|{target_nick}|{target.away}")
+    await client.write(f"CHECK_AFK_OK|{target_nick}|{target.away}")
